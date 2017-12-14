InvictrixROM-Mirror/vendor_invictus

Add a toolchain listing function to envsetup, a less elegant solution Update to work with current iteration of envsetup.sh

build/envsetup.sh
@@ -209,6 +209,28 @@ function hmm() #hidden
     done |column
 }
 
+function tclist {
+dir=prebuilts/gcc/linux-x86/
+    if [[ -f /usr/bin/tree ]]
+       then
+          tree -L 2 $dir -I 'host' |
+          sed s':prebuilts/gcc/linux-x86/:** Toolchain Options **:' |
+          sed s'/aarch64-linux-android-//' |
+          sed s'/aarch64-linux-gnu-//'|
+          sed s'/arm-eabi-//' |
+          sed s'/arm-linux-androideabi-//' |
+          sed s'/arm-linux-gnueabi-//' |
+          sed s'/x86_64-linux-glibc2.15-//' |
+          sed s'/x86_64-w64-mingw32-//' |
+          sed s'/x86_64-linux-android-//';
+     else
+          echo
+          echo 'The binary "tree" is not installed on your system'
+          echo
+     fi
+}
+
 invictus_append_hmm "gzospremote" "Add a git remote for matching gzosp repository"
 invictus_append_hmm "aospremote" "Add git remote for matching AOSP repository"
 invictus_append_hmm "cafremote" "Add git remote for matching CodeAurora repository."
+invictus_append_hmm "tclist"  "List available toolchain options."

CandyROM/vendor_candy

Change-Id: Ie281448d8fa581cac9ee49cf7798601145664cbb

build/envsetup.sh
@@ -185,6 +185,29 @@ function hmm() #hidden
     done |column
 }
 
+function tclist {
+dir=prebuilts/gcc/linux-x86/
+    if [[ -f /usr/bin/tree ]]
+       then
+          tree -L 2 $dir -I 'host' |
+          sed s':prebuilts/gcc/linux-x86/:** Toolchain Options **:' |
+          sed s'/aarch64-linux-android-//' |
+          sed s'/aarch64-linux-gnu-//'|
+          sed s'/arm-eabi-//' |
+          sed s'/arm-linux-androideabi-//' |
+          sed s'/arm-linux-gnueabi-//' |
+          sed s'/x86_64-linux-glibc2.15-//' |
+          sed s'/x86_64-w64-mingw32-//' |
+          sed s'/x86_64-linux-android-//';
+     else
+          echo
+          echo 'The binary "tree" is not installed on your system'
+          echo
+     fi
+}
+
 candy_append_hmm "candyremote" "Add a git remote for matching candy repository"
 candy_append_hmm "aospremote" "Add git remote for matching AOSP repository"
 candy_append_hmm "cafremote" "Add git remote for matching CodeAurora repository."
+candy_append_hmm "tclist"  "List available toolchain options."
+